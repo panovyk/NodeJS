e0183ef2ad8e06076baa27bc0f4ffbe3f0f26548
romashetyt <romashetyt@gmail.com>
2019-10-02 23:26:00 +0300

    HW 1 task

--- Homeworks/HW1 30.09/StudentsModule.js
+++ Homeworks/HW1 30.09/StudentsModule.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-exports.newDir = function (dir1, dir2) {
+exports.newDir = (dir1, dir2) => {
     fs.mkdir(`./${dir1}`, {}, (err) => {
         !err ? console.log('Directory1 CREATED') : console.error('Directory already created!');
 
@@ -13,7 +13,7 @@ exports.newDir = function (dir1, dir2) {
     })
 };
 
-exports.StudentDir = function (group, name, pdata) {
+exports.StudentDir = (group, name, pdata) => {
 
     fs.mkdir(`./node_js/les_1/hw/${group}/${name}`, {}, err => {
         !err? console.log('Created') : console.error('Not created')
