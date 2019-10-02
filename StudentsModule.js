34b3fa2ad99812de1cc9c45144e6d4e6a2f30c76
romashetyt <romashetyt@gmail.com>
2019-10-02 22:04:32 +0300

    HW 1 task

--- StudentsModule.js
+++ StudentsModule.js
@@ -2,32 +2,22 @@ const fs = require('fs');
 
 
 exports.newDir = function (dir1, dir2) {
-    fs.mkdir('./${dir1}', (err) => {
+    fs.mkdir(`./${dir1}`, {}, (err) => {
         !err ? console.log('Directory1 CREATED') : console.error('Directory already created!');
 
     });
 
-    fs.mkdir('./${dir2}', (err) => {
+    fs.mkdir(`./${dir2}`, {},(err) => {
         !err ? console.log('Directory2 CREATED') : console.error('Directory already created!');
 
     })
 };
 
-function NewStudent(group, name) {
-    let jsonedData = JSON.stringify(pdata);
+exports.StudentDir = function (group, name, pdata) {
 
-    fs.writeFile('./${group}./${name}./PersonalData.txt', jsonedData, err => {
-        !err ? console.log(`${name} information added to your data`) : console.error('Data error');
-    })
-}
-
-function NewStudentData(group, name, pdata = {}) {
-    !err ? console.log(`${name} personal data added`) : console.error('Error')
-}
-
-exports.NewStudents = async function (group, name, pdata = {}){
-
-    await NewStudent(group, name);
-
-    NewStudentData (group, name, pdata)
+    fs.mkdir(`./node_js/les_1/hw/${group}/${name}`, {}, err => {
+        !err? console.log('Created') : console.error('Not created')
+    });
+    let writeStreamFile = fs.createWriteStream(`./${group}/${name}txt`);
+    writeStreamFile.write(JSON.stringify(pdata))
 };
--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const {NewStudents, newDir} = require('./NewStudents');
+const {newDir, StudentDir} = require('./StudentsModule');
 
 newDir('jscx-1800', 'jscx-2000');
 
-NewStudents('jscx-1800', 'Atrtur Pirozhkov',
+StudentDir('jscx-1800', 'Atrtur Pirozhkov',
     {name: 'Artur', surname: 'Pirozhkov', skill: 'cook'});
 
-NewStudents('jscx-1800', 'Oksana Owl',
+StudentDir('jscx-1800', 'Oksana Owl',
     {name: 'Oksana', surname: 'Owl', skill: 'not sleep'});
 
-NewStudents('jscx-2000', 'Olya Lama',
+StudentDir('jscx-2000', 'Olya Lama',
     {name: 'Olya', surname: 'Lama', skill: 'eat'});
 
-NewStudents('jscx-2000', 'Pavlo Zibrov',
+StudentDir('jscx-2000', 'Pavlo Zibrov',
     {name: 'Pavlo', surname: 'Zibrov', skill: 'singing'});
