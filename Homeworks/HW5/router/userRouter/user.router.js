19074fe53d4ea322374819f592720b192413d12d
Roman <46422457+panovyk@users.noreply.github.com>
2019-10-19 04:14:42 +0300

    Update user.router.js

--- Homeworks/HW5/router/userRouter/user.router.js
+++ Homeworks/HW5/router/userRouter/user.router.js
@@ -7,6 +7,4 @@ router.post('/', userMiddleware.checkUserValidationMiddleware, user.createUser);
 router.get('/:userID', userMiddleware.isUserPresentedMiddleware, user.getUserById);
 router.patch('/:userID', userMiddleware.isUserPresentedMiddleware, user.updateUser);
 
-
-
 module.exports = router;
